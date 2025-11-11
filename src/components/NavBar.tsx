1802684843e0bf987bd8293c2eb251c22ad890ca
Vadym Terekovskyi <vadihka200192@gmail.com>
2025-11-11 22:21:19 +0200

    Create Navigation Bar component

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,18 @@
+import Link from "next/link";
+
+export default function NavBar() {
+  return (
+    <header className="grid grid-cols-[1fr_auto] items-center font-poppins">
+      <div>
+        <Link href="/" className="text-3xl nav-btn">
+          writings.dev
+        </Link>
+      </div>
+      <div className="text-2xl space-x-[36px]">
+        <Link href="/categories">categories</Link>
+        <Link href="/articles">articles</Link>
+        <Link href="/about">about</Link>
+      </div>
+    </header>
+  );
+}
