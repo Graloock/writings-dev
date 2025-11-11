d981028c2deacccce4b7177db4799b2d5e75c94c
Vadym Terekovskyi <vadihka200192@gmail.com>
2025-11-11 22:22:55 +0200

    create articles page

---
+++ src/app/articles/page.tsx
@@ -0,0 +1,10 @@
+import Logo from "@/components/Logo";
+
+export default function Articles() {
+  return (
+    <main className={""}>
+      <Logo title={"Articles"} />
+      Here&#39;s some articles!
+    </main>
+  );
+}
