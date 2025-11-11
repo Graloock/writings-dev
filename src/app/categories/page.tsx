2106e798fe5867d4819bbf78e94cacd2eb92710f
Vadym Terekovskyi <vadihka200192@gmail.com>
2025-11-11 22:22:47 +0200

    Create categories page

---
+++ src/app/categories/page.tsx
@@ -0,0 +1,10 @@
+import Logo from "@/components/Logo";
+
+export default function Categories() {
+  return (
+    <main>
+      <Logo title={"Categories"} />
+      Here is some Categories!
+    </main>
+  );
+}
