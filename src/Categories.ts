1addcaa4dbf3c0767ccb2d665a62aae47572a4e2
Vadym Terekovskyi <vadihka200192@gmail.com>
2025-11-11 22:16:45 +0200

    Move list to a separate file in a root of /src

---
+++ src/Categories.ts
@@ -0,0 +1,32 @@
+import { Categories } from "@/components/CategoriesBar";
+
+export const categories: Categories[] = [
+  {
+    label: "JavaScript",
+    href: "blog/javascript",
+  },
+  {
+    label: "DevOps",
+    href: "blog/devops",
+  },
+  {
+    label: "Cloud",
+    href: "blog/cloud",
+  },
+  {
+    label: "Terraform",
+    href: "blog/terraform",
+  },
+  {
+    label: "Architecture",
+    href: "blog/architecture",
+  },
+  {
+    label: "Scalability",
+    href: "blog/scalability",
+  },
+  {
+    label: "Explainers",
+    href: "blog/explainers",
+  },
+];
