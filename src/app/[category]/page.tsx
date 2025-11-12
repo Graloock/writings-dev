43fc92727bdddf4ad7cc7476bc5f889db4b97fb0
Vadym Terekovskyi <vadihka200192@gmail.com>
2025-11-12 22:25:09 +0200

    Change to highlighted variable

--- src/app/[category]/page.tsx
+++ src/app/[category]/page.tsx
@@ -18,7 +18,7 @@ export default async function page({
   return (
     <div>
       <Logo title={categoryLabel} />
-      <CategoriesBar categories={categories} />
+      <CategoriesBar categories={categories} highlighted={category} />
     </div>
   );
 }
