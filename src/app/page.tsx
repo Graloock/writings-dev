b34f04fcb15ef8be7a6303530530fb341a396f6d
Vadym Terekovskyi <vadihka200192@gmail.com>
2025-11-16 21:59:18 +0200

    Map objects into a posts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,17 +2,35 @@ import Logo from "@/components/layout/Logo";
 import CategoriesBar from "@/components/layout/CategoriesBar";
 import { categories } from "@/Categories";
 import Separator from "@/components/decorator/Separator";
+import { Posts } from "@/Posts";
+import PostElement from "@/components/ui/PostElement";
 
 export default function Home() {
   return (
-    <main className="">
+    <main className="w-full">
       <Logo
         title={"Writings for Developers"}
         subtitle={"Curated collection of articles for busy developers"}
       />
       <Separator />
       <CategoriesBar categories={categories} />
-      Hi!
+      <div
+        className={
+          "grid sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-[37px]"
+        }
+      >
+        {Posts.map(({ image, alt, title, description, date, category }) => (
+          <PostElement
+            image={image}
+            key={alt}
+            alt={alt}
+            title={title}
+            description={description}
+            date={date}
+            category={category}
+          />
+        ))}
+      </div>
     </main>
   );
 }
