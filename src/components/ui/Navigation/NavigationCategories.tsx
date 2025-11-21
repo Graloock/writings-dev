3d2f00d94ff3ec8d669410e082426a7f1f8c73b8
Vadym Terekovskyi <vadihka200192@gmail.com>
2025-11-22 01:45:24 +0200

    Make Categories in NavigationBar modular

---
+++ src/components/ui/Navigation/NavigationCategories.tsx
@@ -0,0 +1,39 @@
+import GradientAccent from "@/components/layout/GradientAccent";
+import Link from "next/link";
+import { Category } from "@/Categories";
+
+interface NavigationCategoriesProps extends React.HTMLProps<HTMLDivElement> {
+  categories: Category[];
+  highlighted?: string;
+}
+
+export default function NavigationCategories({
+  categories,
+  highlighted,
+}: NavigationCategoriesProps) {
+  return (
+    <div className={"overflow-x-clip relative"}>
+      <GradientAccent direction={"left"} />
+      <div
+        className={
+          "space-x-9 text-[27px] overflow-x-auto font-recursive pr-20 pl-10"
+        }
+      >
+        {categories.map((category) => (
+          <Link
+            href={category.href}
+            key={category.label}
+            className={
+              category.label.toLowerCase() === highlighted
+                ? "text-[#DCDCDC]"
+                : "text-[#909090]"
+            }
+          >
+            {category.label}
+          </Link>
+        ))}
+      </div>
+      <GradientAccent direction={"right"} />
+    </div>
+  );
+}
