001ceccf3d1b995ea0946aca20d42185b07d133c
Vadym Terekovskyi <vadihka200192@gmail.com>
2025-11-22 19:03:32 +0200

    Prod fixes

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  basePath: "/writings.dev", // Make pictures work again
+  output: "export", // Enable static exports
   reactCompiler: true,
 };
 
