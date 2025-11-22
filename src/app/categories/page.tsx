dc90ffe011fc6e2eab9b25f0e1f6084d91de042a
Vadym Terekovskyi <vadihka200192@gmail.com>
2025-11-22 02:06:39 +0200

    Actually make categories page a bit more useful

--- src/app/categories/page.tsx
+++ src/app/categories/page.tsx
@@ -1,10 +1,12 @@
 import Logo from "@/components/layout/Logo";
+import NavigationCategories from "@/components/ui/Navigation/NavigationCategories";
+import { categories } from "@/Categories";
 
 export default function Categories() {
   return (
     <main>
       <Logo title={"Categories"} />
-      Here is some Categories!
+      <NavigationCategories categories={categories} />
     </main>
   );
 }
