95c22b8253242555e479d7d09b690359162e5014
Vadym Terekovskyi <vadihka200192@gmail.com>
2025-11-22 02:04:21 +0200

    Make font smaller for mobile devices

--- src/components/layout/Logo.tsx
+++ src/components/layout/Logo.tsx
@@ -1,25 +1,22 @@
-import Image from "next/image";
+import LogoIcon from "@/icons/LogoIcon";
 
-export default function Logo({
-  title,
-  subtitle,
-}: {
+interface LogoProps extends React.HTMLAttributes<HTMLDivElement> {
   title?: string;
   subtitle?: string;
-}) {
+}
+
+export default function Logo({ title, subtitle, ...rest }: LogoProps) {
   return (
-    <div className="grid justify-items-center pt-[76px]">
-      <Image
-        className={"mb-10"}
-        src="/logo.svg"
-        alt={"Logo"}
-        width={194.45}
-        height={204.08}
-      ></Image>
-      <h1 className={"font-recursive text-[#FF7575] text-[71px] text-center"}>
+    <div className="grid justify-items-center pt-[76px]" {...rest}>
+      <LogoIcon width={194.45} height={204.08} />
+      <h1
+        className={
+          "font-recursive text-[#FF7575] text-5xl sm:text-7xl text-center"
+        }
+      >
         {title}
       </h1>
-      <h2 className={"text-[29px] text-center mb-20"}>{subtitle}</h2>
+      <h2 className={"text-2xl sm:text-3xl text-center mb-20"}>{subtitle}</h2>
     </div>
   );
 }
