d5dc2f6928b9aedb18228ada48e421f1f93d6bbe
Vadym Terekovskyi <vadihka200192@gmail.com>
2025-11-22 02:06:14 +0200

    More modularity

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
 import Logo from "@/components/layout/Logo";
-import CategoriesBar from "@/components/layout/CategoriesBar";
+import NavigationBar from "@/components/ui/Navigation/NavigationBar";
 import { categories } from "@/Categories";
-import Separator from "@/components/decorator/Separator";
+import Separator from "@/components/layout/Separator";
 import { Posts } from "@/Posts";
-import PostElement from "@/components/ui/PostElement";
+import getPosts from "@/lib/post/getPosts";
 
 export default function Home() {
   return (
@@ -13,23 +13,13 @@ export default function Home() {
         subtitle={"Curated collection of articles for busy developers"}
       />
       <Separator />
-      <CategoriesBar categories={categories} />
+      <NavigationBar categories={categories} />
       <div
         className={
           "grid sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-[37px]"
         }
       >
-        {Posts.map(({ image, alt, title, description, date, category }) => (
-          <PostElement
-            image={image}
-            key={alt}
-            alt={alt}
-            title={title}
-            description={description}
-            date={date}
-            category={category}
-          />
-        ))}
+        {getPosts(Posts)}
       </div>
     </main>
   );
