166e71381aed714aafcfd898b3dbaf1105e4ce83
Vadym Terekovskyi <vadihka200192@gmail.com>
2025-11-30 01:23:19 +0200

    Clicking Logo sends to homepage

--- src/components/ui/Logo.tsx
+++ src/components/ui/Logo.tsx
@@ -1,4 +1,5 @@
 import LogoIcon from "@/icons/LogoIcon";
+import Link from "next/link";
 
 interface LogoProps extends React.HTMLAttributes<HTMLDivElement> {
   title?: string;
@@ -8,7 +9,9 @@ interface LogoProps extends React.HTMLAttributes<HTMLDivElement> {
 export default function Logo({ title, subtitle, ...rest }: LogoProps) {
   return (
     <div className="grid justify-items-center pt-[76px]" {...rest}>
-      <LogoIcon width={194.45} height={204.08} />
+      <Link href="/">
+        <LogoIcon width={194.45} height={204.08} />
+      </Link>
       <h1
         className={
           "font-recursive text-[#FF7575] text-5xl sm:text-7xl text-center"
