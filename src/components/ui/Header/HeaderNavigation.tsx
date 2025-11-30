43451375e043d75d325aa83af064b845b8c161eb
Vadym Terekovskyi <vadihka200192@gmail.com>
2025-11-30 03:17:27 +0200

    Add className variable

--- src/components/ui/Header/HeaderNavigation.tsx
+++ src/components/ui/Header/HeaderNavigation.tsx
@@ -1,8 +1,12 @@
 import Link from "next/link";
 
-export default function HeaderNavigation() {
+export default function HeaderNavigation({
+  className,
+}: {
+  className?: string;
+}) {
   return (
-    <div className="text-2xl space-x-[36px]">
+    <div className={`text-2xl space-x-[36px] ${className}`}>
       <Link href="/categories">categories</Link>
       <Link href="/articles">articles</Link>
       <Link href="/about">about</Link>
