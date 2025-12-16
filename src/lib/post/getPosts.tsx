8aef5bca1911cf002289411ffb52b6d7de2c0d5a
Vadym Terekovskyi <vadihka200192@gmail.com>
2025-12-16 21:56:15 +0200

    Make pagination

--- src/lib/post/getPosts.tsx
+++ src/lib/post/getPosts.tsx
@@ -1,18 +1,24 @@
-import { PostProps } from "@/Posts";
-import Post from "@/components/ui/Post/Post";
+import Post from "@/components/ui/post/Post";
+import { PostQuery } from "@/components/layout/post/Posts";
 
-export default function getPosts(Posts: PostProps[], category?: string) {
-  return Posts.filter((post) =>
-    category ? post.category === category : true,
-  ).map(({ image, alt, title, description, date, category }) => (
-    <Post
-      image={image}
-      key={alt}
-      alt={alt}
-      title={title}
-      description={description}
-      date={date}
-      category={category}
-    />
-  ));
+export default function getPosts({
+  posts,
+  page = 1,
+  postsPerPage = 9,
+  category,
+}: PostQuery) {
+  return posts
+    .filter((post) => (category ? post.category === category : true))
+    .splice((page - 1) * postsPerPage, postsPerPage)
+    .map(({ image, alt, title, description, date, category }) => (
+      <Post
+        image={image}
+        key={alt}
+        alt={alt}
+        title={title}
+        description={description}
+        date={date}
+        category={category}
+      />
+    ));
 }
