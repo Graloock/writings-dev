275551a265fb1975a69dc4cff434b6bdccc6eb5e
Vadym Terekovskyi <vadihka200192@gmail.com>
2025-12-16 22:59:46 +0200

    Try without export

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   basePath: "/writings-dev", // For project to know where it is
-  output: "export", // Enable static exports for GH Pages
   reactCompiler: true,
 };
 
