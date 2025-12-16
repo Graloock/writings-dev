471d940715c7c8807ba3e7ac3e15335c87e4813a
Vadym Terekovskyi <vadihka200192@gmail.com>
2025-12-16 22:34:56 +0200

    Refactor

--- src/components/ui/header/HeaderNavigation.tsx
+++ src/components/ui/header/HeaderNavigation.tsx
@@ -1,12 +1,15 @@
 import Link from "next/link";
 
+interface HeaderNavigationProps extends React.HTMLProps<HTMLDivElement> {
+  className?: string;
+}
+
 export default function HeaderNavigation({
   className,
-}: {
-  className?: string;
-}) {
+  ...rest
+}: HeaderNavigationProps) {
   return (
-    <div className={`text-2xl space-x-[36px] ${className}`}>
+    <div className={`text-2xl ${className}`} {...rest}>
       <Link href="/categories">categories</Link>
       <Link href="/articles">articles</Link>
       <Link href="/about">about</Link>
