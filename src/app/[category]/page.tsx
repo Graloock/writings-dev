e55eb1867dc9c3f12892b08379ca00ea4fc51e1d
Vadym Terekovskyi <vadihka200192@gmail.com>
2025-12-16 21:59:26 +0200

    Remade Posts element, add postQuery variable

--- src/app/[category]/page.tsx
+++ src/app/[category]/page.tsx
@@ -1,11 +1,11 @@
 import Logo from "@/components/ui/Logo";
-import NavigationBar from "@/components/ui/Navigation/NavigationBar";
+import NavigationBar from "@/components/ui/navigation/NavigationBar";
 import { categories } from "@/Categories";
 import findCategoryLabel from "@/lib/util/findCategoryLabel";
 import { notFound } from "next/navigation";
 import Separator from "@/components/layout/Separator";
-import getPosts from "@/lib/post/getPosts";
-import { Posts } from "@/Posts";
+import { PostList } from "@/PostList";
+import Posts, { PostQuery } from "@/components/layout/post/Posts";
 
 export function generateStaticParams() {
   return categories.map(({ href }) => ({
@@ -15,25 +15,29 @@ export function generateStaticParams() {
 
 export default async function page({
   params,
+  searchParams,
 }: {
   params: Promise<{ category: string }>;
+  searchParams: Promise<{ page: number }>;
 }) {
   const { category } = await params;
+  const page = (await searchParams).page;
   const categoryLabel = findCategoryLabel(category)
     ? findCategoryLabel(category)
     : notFound();
+
+  const posts: PostQuery = {
+    posts: PostList,
+    page,
+    category: categoryLabel,
+  };
+
   return (
     <div className={"w-full"}>
       <Logo title={categoryLabel} />
       <Separator />
-      <NavigationBar categories={categories} highlighted={category} />
-      <div
-        className={
-          "grid sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-[37px]"
-        }
-      >
-        {getPosts(Posts, categoryLabel)}
-      </div>
+      <NavigationBar categories={categories} selectedCategory={category} />
+      <Posts {...posts} />
     </div>
   );
 }
