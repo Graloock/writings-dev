ef00ea594be12adf1ccddd1173b6cf2de195cab7
Vadym Terekovskyi <vadihka200192@gmail.com>
2025-12-16 22:34:41 +0200

    Add Source Sans 3 font, footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,15 @@
 import type { Metadata } from "next";
-import { Recursive, Poppins } from "next/font/google";
+import { Recursive, Poppins, Source_Sans_3 } from "next/font/google";
 import "@/css/globals.css";
-import Header from "@/components/ui/Header/Header";
+import Header from "@/components/ui/header/Header";
 import Separator from "@/components/layout/Separator";
 import React from "react";
+import Footer from "@/components/ui/footer/Footer";
+
+const sourceSans = Source_Sans_3({
+  weight: ["600"],
+  variable: "--font-source-sans",
+});
 
 const poppins = Poppins({
   weight: ["400", "600"],
@@ -30,12 +36,13 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${recursive.variable} ${poppins.variable} margin antialiased`}
+        className={`${recursive.variable} ${poppins.variable} ${sourceSans.variable} margin antialiased`}
       >
         <div>
           <Header />
           {children}
           <Separator className={"my-3"} />
+          <Footer />
         </div>
       </body>
     </html>
